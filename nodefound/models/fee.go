5480b6146c18019292e4b68e63795e93103e2950
calvindc <466862683@qq.com>
2024-08-08 19:03:34 +0800

    init nodefound

--- nodefound/models/fee.go
+++ nodefound/models/fee.go
@@ -7,6 +7,15 @@ import (
 	"math/big"
 )
 
+const (
+	//FeePolicyConstant 每笔交易,不论金额,固定收费
+	FeePolicyConstant = iota
+	//FeePolicyPercent 每笔交易固定收取一定比例的费用
+	FeePolicyPercent
+	//FeePolicyCombined 以上两种方式的组合
+	FeePolicyCombined
+)
+
 // AccountFee 账户的缺省收费
 type AccountFee struct {
 	Account         string `gorm:"primary_key"`
@@ -23,6 +32,17 @@ type AccountTokenFee struct {
 	FeePolicy       int
 	FeeConstantPart string
 	FeePercentPart  int64
+}
+
+// ChannelParticipantFee 存储通道一方的收费信息
+type ChannelParticipantFee struct {
+	ID              int
+	ChannelID       string `gorm:"index"`
+	Participant     string `gorm:"index"`
+	Token           string
+	FeePolicy       int
+	FeeConstantPart string //固定部分是一个整数,比如一次收取1token
+	FeePercentPart  int64  //0表示不收费,1000表示收费千分之一
 }
 
 // TokenFee 针对某种token 的缺省收费,暂不启用
@@ -54,7 +74,7 @@ func UpdataAccountDefaultFeePolicy(account common.Address, fee *Fee, db *gorm.DB
 		FeeConstantPart: bigInt2String(fee.FeeConstant),
 		FeePercentPart:  fee.FeePercent,
 	}
-	err := db.Where(&AccountFee{Account: account.String()}).Find(&AccountFee{}).Error
+	err := db.Find(&AccountFee{}, "account=?", account.String()).Error
 	if err == nil {
 		return db.Save(acc).Error
 	}
@@ -65,7 +85,7 @@ func UpdataAccountDefaultFeePolicy(account common.Address, fee *Fee, db *gorm.DB
 // GetAccountFeePolicy 获取某个账户的缺省收费,新创建的通道都会按照此缺省设置进行
 func GetAccountFeePolicy(account common.Address, db *gorm.DB) (fee *Fee) {
 	a := &AccountFee{}
-	err := db.Where(&AccountFee{Account: account.String()}).Find(a).Error
+	err := db.First(&a, "account = ?", account.String()).Error
 	if err == nil {
 		return &Fee{
 			FeePolicy:   a.FeePolicy,
@@ -94,4 +114,43 @@ func GetAccountTokenFee(account, token common.Address, db *gorm.DB) (fee *Fee, e
 		}
 	}
 	return
+}
+
+func UpdateAccountTokenFee(account, token common.Address, fee *Fee, db *gorm.DB) error {
+	atf := &AccountTokenFee{
+		Token:   token.String(),
+		Account: account.String(),
+	}
+
+	err := db.Where(atf).Find(atf).Error
+	atf.FeePolicy = fee.FeePolicy
+	atf.FeeConstantPart = bigInt2String(fee.FeeConstant)
+	atf.FeePercentPart = fee.FeePercent
+	if err == nil {
+		return db.Save(atf).Error
+	}
+	return db.Create(atf).Error
+
+}
+
+func DeleteAccountAllFeeRate(account common.Address, db *gorm.DB) (err error) {
+	tx := db.Begin()
+	if err != nil {
+		tx.Rollback()
+	}
+
+	err2 := tx.Where("account=?", account.String()).Delete(&AccountFee{}).Error
+	if err2 != nil {
+		return err2
+	}
+	err3 := tx.Where("account=?", account.String()).Delete(&AccountTokenFee{}).Error
+	if err3 != nil {
+		return err3
+	}
+
+	err4 := tx.Where("participant=?", account.String()).Delete(&ChannelParticipantFee{}).Error
+	if err4 != nil {
+		return err4
+	}
+	return tx.Commit().Error
 }
---
+++ nodefound/models/fee_test.go
@@ -0,0 +1,88 @@
+package models
+
+import (
+	"fmt"
+	"github.com/SmartMeshFoundation/Photon/utils"
+	"github.com/calvindc/comm-x/nodefound/config"
+	"github.com/calvindc/comm-x/orm-db"
+	"gorm.io/gorm"
+	"math/big"
+	"os"
+	"path"
+	"path/filepath"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+var vdb *gorm.DB
+
+func TestGetAccountFeePolicy(t *testing.T) {
+	filepath.Walk(os.TempDir(), func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.IsDir() {
+			return nil
+		}
+		if strings.HasPrefix(filepath.Base(path), "nodefound") {
+			return os.Remove(path)
+		}
+		return nil
+	})
+
+	dbPath := path.Join(os.TempDir(), fmt.Sprintf("nodefound%s.db", utils.RandomString(10)))
+	t.Logf("dbpath=%s", dbPath)
+	err := os.Remove(dbPath) //use one db
+	if err != nil {
+		t.Logf("remove err %s", err)
+	}
+	vdb, err := orm_db.SetupDB("sqlite3", dbPath, &AccountFee{}, &AccountTokenFee{}, &ChannelParticipantFee{}, &TokenFee{})
+	if err != nil {
+		t.Logf("setupDB err %s", err)
+	}
+	address := utils.NewRandomAddress()
+	fee := GetAccountFeePolicy(address, vdb)
+	if fee.FeePolicy != config.DefaultFeePolicy || fee.FeePercent != config.DefaultFeePercentPart {
+		t.Errorf("not equal default")
+		return
+	}
+	//give some-self define
+	fee.FeePolicy = FeePolicyPercent
+	fee.FeePercent = 0
+	fee.FeeConstant = big.NewInt(30)
+
+	err = UpdataAccountDefaultFeePolicy(address, fee, vdb)
+	if err != nil {
+		t.Errorf("UpdataAccountDefaultFeePolicy err %s", err)
+		return
+	}
+
+	fee.FeePercent = 50
+	err = UpdataAccountDefaultFeePolicy(address, fee, vdb)
+	if err != nil {
+		t.Errorf("UpdataAccountDefaultFeePolicy err %s", err)
+		return
+	}
+
+	//============
+	fee2 := GetAccountFeePolicy(address, vdb)
+	if !reflect.DeepEqual(fee, fee2) {
+		t.Error("not equal")
+		return
+	}
+
+	//============
+	err = DeleteAccountAllFeeRate(address, vdb)
+	if err != nil {
+		t.Errorf("DeleteAccountAllFeeRate err %s", err)
+		return
+	}
+
+	fee = GetAccountFeePolicy(address, vdb)
+	if fee.FeePolicy != config.DefaultFeePolicy || fee.FeePercent != config.DefaultFeePercentPart {
+		t.Error("not equal default")
+		return
+	}
+
+}
